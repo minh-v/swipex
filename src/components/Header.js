2fe0f9383245bf6b2918ab42444304ed7f8b213b
Minh Vu <minhhv1@uci.edu>
2020-02-04 00:06:21 -0800

    removed search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,19 +8,10 @@ class Header extends Component {
   render() {
     return (
       <Container className="pb-5">
-        {/*Navbar with name of app and search bar to the right, temp seperation method: className="justify-content-between" */}
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="#home" id="Navbar">
             SwipeX
           </Navbar.Brand>
-          <Form inline className="ml-auto">
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className="mr-sm-2"
-            />
-            <Button variant="outline-info">Search</Button>
-          </Form>
         </Navbar>
       </Container>
     );
